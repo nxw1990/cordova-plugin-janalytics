063deb6ae28934cb13a686960abf44084149ee50
xiuwei.nie <xiuwei.nie@bitmain.com>
2019-04-15 17:58:30 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# cordova-plugin-janalytics
+
+极光官方提供的[极光统计](https://www.jiguang.cn/analytics) Cordova 插件。
+
+## Install
+
+- 通过 cordova plugin 安装，要求 Cordova CLI 5.0+：
+
+  ```shell
+  cordova plugin add cordova-plugin-janalytics --variable APP_KEY=极光appKey
+  ```
+
+- 或直接通过 url 安装：
+
+  ```shell
+  cordova plugin add https://github.com/jpush/cordova-plugin-janalytics --variable APP_KEY=极光appKey
+  ```
+
+- 或下载到本地安装：
+
+  ```shell
+  cordova plugin add <plugin_local_path> --variable APP_KEY=极光appKey
+  ```
+
+## API
+
+可直接参考 [JAnalytics.js](/www/JAnalytics.js) 文件。
+
+## 参考资料
+
+[官方文档](https://docs.jiguang.cn/janalytics/guideline/intro/)
+
+## Support
+
+- [极光社区](http://community.jiguang.cn/)
+- QQ 交流群：413602425 / 524248013
+
+## License
+
+MIT © [JiGuang](/license)
---
+++ www/JAnalytics.js
@@ -0,0 +1,125 @@
+var exec = require('cordova/exec')
+
+var PLUGIN_NAME = 'JAnalyticsPlugin'
+
+var JAnalytics = {
+  init: function () {
+    exec(null, null, PLUGIN_NAME, 'init', [])
+  },
+  /**
+   * 是否开启 debug 模式（开启后会打印更多调试信息）。
+   *
+   * @param {object} params = {'enable': Boolean}
+   */
+  setDebugMode: function (params) {
+    exec(null, null, PLUGIN_NAME, 'setDebugMode', [params])
+  },
+  /**
+   * 开启 crash log 上报（默认关闭）。
+   */
+  initCrashHandler: function () {
+    exec(null, null, PLUGIN_NAME, 'initCrashHandler', [])
+  },
+  /**
+   * 关闭 crash log 上报（目前 iOS 无法关闭）。
+   */
+  stopCrashHandler: function () {
+    exec(null, null, PLUGIN_NAME, 'stopCrashHandler', [])
+  },
+  /**
+   * @param {object} params = {
+   *  'pageName': String  // 页面名称，和 onPageEnd 需要成对调用。
+   * }
+   */
+  onPageStart: function (params) {
+    exec(null, null, PLUGIN_NAME, 'onPageStart', [params])
+  },
+  /**
+   * @param {object} params = {
+   *  'pageName': String  // 页面名称，和 onPageStart 需要成对调用。
+   * }
+   */
+  onPageEnd: function (params) {
+    exec(null, null, PLUGIN_NAME, 'onPageEnd', [params])
+  },
+  /**
+   * 添加计数事件。
+   *
+   * @param {object} params = {
+   *  'eventId': String,  // 事件 id
+   *  'extras': object    // Optional. 扩展参数，类似 {'key1': String, 'key2': String}
+   * }
+   */
+  addCountEvent: function (params) {
+    exec(null, null, PLUGIN_NAME, 'addCountEvent', [params])
+  },
+  /**
+   * 添加计算事件。
+   * 
+   * @param {object} params = {
+   *  'eventId': String,    // 事件 id
+   *  'eventValue': Number, // 事件的值
+   *  'extras': object      // Optional. 扩展参数，类似 {'key1': 'value1'}
+   * }
+   */
+  addCalculateEvent: function (params) {
+    exec(null, null, PLUGIN_NAME, 'addCalculateEvent', [params])
+  },
+  /**
+   * 添加登录事件。
+   * 
+   * @param {object} params = {
+   *  'loginMethod': String,     // 登录方式
+   *  'isLoginSuccess': Boolean, // 是否登录成功
+   *  'extras': object           // Optional. 扩展参数，类似 {'key1': 'value1'}
+   * }
+   */
+  addLoginEvent: function (params) {
+    exec(null, null, PLUGIN_NAME, 'addLoginEvent', [params])
+  },
+  /**
+   * 添加注册事件。
+   * 
+   * @param {object} params = {
+   *  'registerMethod': String,     // 登录方式
+   *  'isRegisterSuccess': Boolean, // 是否登录成功
+   *  'extras': object              // Optional. 扩展参数，类似 {'key1': 'value1'}
+   * }
+   */
+  addRegisterEvent: function (params) {
+    exec(null, null, PLUGIN_NAME, 'addRegisterEvent', [params])
+  },
+  /**
+   * 添加浏览事件。
+   * 
+   * @param {object} params = {
+   *  'browseId': String,       // 浏览内容 id
+   *  'browseName': String,     // 内容名称
+   *  'browseType': String,     // 内容类型
+   *  'browseDuration': Number, // 浏览时长，单位秒
+   *  'extras': object          // Optional. 扩展参数，类似 {'key1': 'value1'}
+   * }
+   */
+  addBrowseEvent: function (params) {
+    exec(null, null, PLUGIN_NAME, 'addBrowseEvent', [params])
+  },
+  /**
+   * 添加支付事件。
+   * 
+   * @param {object} params = {
+   *  'goodsId': String,            // 商品 id
+   *  'goodsName': String,          // 商品名称
+   *  'price': Number,              // 商品价格
+   *  'currency': String,           // 货币类型：'CNY' / 'USD'
+   *  'isPurchaseSuccess': Boolean, // 是否支付成功
+   *  'goodsType': String,          // 商品类型
+   *  'goodsCount': Number,         // 商品数量
+   *  'extras': object              // Optional. 扩展参数，类似 {'key1': 'value1'}
+   * }
+   */
+  addPurchaseEvent: function (params) {
+    exec(null, null, PLUGIN_NAME, 'addPurchaseEvent', [params])
+  }
+}
+
+module.exports = JAnalytics
